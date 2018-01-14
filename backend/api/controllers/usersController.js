9020705dae6e4b4a9d4b9f4a128f6cdf4b02df2e
Catalin Lungu <catalin1122@gmail.com>
2018-01-14 19:24:56 +0200

    Added support for filtering users by query

--- backend/api/controllers/usersController.js
+++ backend/api/controllers/usersController.js
@@ -4,17 +4,20 @@ const esService = require('../services/esService'),
     uuidv4 = require('uuid/v4');
 
 exports.listUsers = function (req, res) {
-    esService.searchUsers("*", req.params.from, req.params.size)
+    esService.searchUsers(req.query.q, req.query.from, req.query.size)
         .then((response, error) => {
             if (error)
                 res.status(500).json(error);
-            res.status(200).json(response.hits.hits.map(hit => {
-                let result = {};
-                for (let property in hit._source)
-                    if (property != "password")
-                        result[property] = hit._source[property];
-                return result;
-            }));
+
+            if (response.hits.total > 0)
+                res.status(200).json({
+                    total: response.hits.total,
+                    hits: response.hits.hits.map(hit => {
+                        delete hit._source.password;
+                        return hit._source;
+                    })
+                });
+            else res.status(204).send();
         });
 };
 
