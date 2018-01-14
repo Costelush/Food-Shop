c2b8ca5bebc4c7f0b7000a2ebba95d741ece6672
Catalin Lungu <catalin1122@gmail.com>
2018-01-14 11:49:51 +0200

    added fileUpload use into server.js to allow file uploads

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
-var express = require('express'),
+const express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000
+  port = process.env.PORT || 3000,
   bodyParser = require('body-parser'),
-  routes = require('./api/routes/shopRoutes');
+  routes = require('./api/routes/shopRoutes'),
+  fileUpload = require('express-fileupload');;
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(fileUpload({
+  limits: {
+    fileSize: 1 * 1024 * 1024,
+    fields: 50,
+    files: 6,
+    parts: 51,
+  }
+}));
 
 routes(app);
 app.listen(port);
