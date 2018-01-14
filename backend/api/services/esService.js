422449a802fc2f5d1c280acad0e1d33de8a03ba7
Catalin Lungu <catalin1122@gmail.com>
2018-01-14 11:46:11 +0200

    modified esService to better use search and index functions

--- backend/api/services/esService.js
+++ backend/api/services/esService.js
@@ -2,32 +2,50 @@ var elasticsearch = require('elasticsearch'),
     config = require('../../config/es-config.json'),
     validationUtils = require('../utils/validationUtils');
 
-class EsService {
-    constructor() {
-        this.client = new elasticsearch.Client({
-            host: config.host,
-            log: config.logLevel
-        });
-    }
+const esClient = new elasticsearch.Client({
+    host: config.host,
+    log: config.logLevel
+});
 
-    search(index, query, from, size) {
-        let pagination = validationUtils.validatePagination(from, size);
-        return this.client.search({
-            index: index,
-            q: query,
-            from: pagination.from,
-            size: pagination.size
-        });
-    }
+exports.search = function (index, query, from, size) {
+    let pagination = validationUtils.validatePagination(from, size);
+    console.log("Searching for " + index);
+    return esClient.search({
+        index: index,
+        q: query,
+        from: pagination.from,
+        size: pagination.size
+    });
+};
 
-    index(index, body) {
-        return this.client.index({
-            index: index,
-            type: 'doc',
-            body: body
-        });
-    }
-}
+exports.searchUsers = function (query, from, size) {
+    return this.search(config.usersIndex, query, from, size);
+};
 
-// export the class
-module.exports = EsService;
+exports.searchProducts = function (query, from, size) {
+    return this.search(config.productsIndex, query, from, size);
+};
+
+exports.searchInvoices = function (query, from, size) {
+    return this.search(config.invoicesIndex, query, from, size);
+};
+
+exports.index = function (index, body) {
+    return esClient.index({
+        index: index,
+        type: 'doc',
+        body: body
+    });
+};
+
+exports.indexUser = function (body) {
+    return this.index(config.usersIndex, body);
+};
+
+exports.indexProduct = function (body) {
+    return this.index(config.productsIndex, body);
+};
+
+exports.indexInvoice = function (body) {
+    return this.index(config.invoicesIndex, body);
+};
