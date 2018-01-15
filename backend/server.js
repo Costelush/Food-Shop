cb8032afa5ca9c99f56175409d3e68adc79b6333
Catalin Lungu <catalin1122@gmail.com>
2018-01-15 10:24:50 +0200

    Added CORS support

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,17 @@ const express = require('express'),
   routes = require('./api/routes/shopRoutes'),
   fileUpload = require('express-fileupload'),
   cookieParser = require('cookie-parser'),
-  session = require('express-session');
+  session = require('express-session'),
+  cors = require('cors');
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.json());
 
